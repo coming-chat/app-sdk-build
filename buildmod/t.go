43e18cb5632abab0cb80de5b6fb5551fd35fadda
xiang-xx <xiang9311@gmail.com>
2022-08-23 14:55:37 +0800

    build: fix go mod

--- buildmod/t.go
+++ buildmod/t.go
@@ -1,14 +1,12 @@
 package buildmod
 
-import (
-	"github.com/coming-chat/go-defi-sdk/util"
-	"github.com/coming-chat/go-red-packet/redpacket"
-	"github.com/coming-chat/wallet-SDK/core/aptos"
-)
+import "github.com/coming-chat/wallet-SDK/core/aptos"
 
-const a = redpacket.AptosDecimal
-const b = aptos.GasPrice
-const c = util.ErrAddress
+var A int
+
+func init() {
+	A = aptos.MaxGasAmount
+}
 
 type T struct {
 }
